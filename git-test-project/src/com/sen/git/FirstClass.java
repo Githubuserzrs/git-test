7b4df18450467b818650fee62152dc1447025c01
森玄页码 <1689909273@qq.com>
2021-01-22 23:24:57 +0800

    把本地工程添加到git2

---
+++ git-test-project/src/com/sen/git/FirstClass.java
@@ -0,0 +1,16 @@
+package com.sen.git;
+
+/**
+ * @author Zrs
+ * @company At home
+ * @date 2021/1/22 23:23
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git-project");
+        System.out.println("hello git-project");
+        System.out.println("hello git-project");
+        System.out.println("hello git-project");
+        System.out.println("hello git-project");
+    }
+}
