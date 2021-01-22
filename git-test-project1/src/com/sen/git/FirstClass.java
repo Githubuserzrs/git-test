9faef6e8fc405699c905a3c89d1e9088bfeb97be
森玄页码 <1689909273@qq.com>
2021-01-22 23:52:28 +0800

    修稿FristClass文件

--- git-test-project1/src/com/sen/git/FirstClass.java
+++ git-test-project1/src/com/sen/git/FirstClass.java
@@ -9,4 +9,8 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("412121as2dasd");
     }
+    public void test(){
+        int i = 0;
+        i += 10;
+    }
 }
